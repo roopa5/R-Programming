f17b7b3b558704d4cb41ead07e4d1650242ac74c
roopa5 <roopakadavil@icloud.com>
2016-06-16 22:39:41 +0000

    R Programming Assignment 3 - cacheMatrix

---
+++ cachemat.R
@@ -0,0 +1,29 @@
+##function for creating matrix 
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+##function for solving the inverse
+
+cacheSolve <- function(x = matrix(), ...) {
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+}
